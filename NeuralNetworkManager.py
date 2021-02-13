60226d873c0c5d50877752f0d97965037bf2ce8b
Daniel <danielwz99@gmail.com>
2021-02-13 16:43:59 +0100

    added nn with more filters

--- NeuralNetworkManager.py
+++ NeuralNetworkManager.py
@@ -24,3 +24,19 @@ class NeuralNetworkManager:
         model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 
         yield NeuralNetwork("Simple CNN", model)
+
+        model = Sequential()
+        model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)))
+        model.add(MaxPooling2D((2, 2)))
+        model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+        model.add(MaxPooling2D((2, 2)))
+        model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+        model.add(MaxPooling2D((2, 2)))
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+        model.add(Dense(10, activation='softmax'))
+
+        opt = SGD(lr=0.001, momentum=0.9)
+        model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+
+        yield NeuralNetwork("Simple CNN with more filters", model)
