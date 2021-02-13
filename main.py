bc174b978227a64b95dffcddbe8609e8f723bcc3
Daniel <danielwz99@gmail.com>
2021-02-13 14:39:39 +0100

    passing nns to compare workflow

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from NeuralNetworkComparer import NeuralNetworkComparer
+from NeuralNetworkManager import NeuralNetworkManager
 
-NeuralNetworkComparer.compare_neural_networks(None)
+
+def main():
+    NeuralNetworkComparer.compare_neural_networks(NeuralNetworkManager.get_all_networks())
+
+
+if __name__ == "__main__":
+    main()
