c31556305a2b9457bea17638395ee9df3410959a
Daniel <danielwz99@gmail.com>
2021-02-13 16:49:00 +0100

    refactor and addind flat cnn

--- NeuralNetworkManager.py
+++ NeuralNetworkManager.py
@@ -14,15 +14,23 @@ class NeuralNetworkManager:
         model.add(MaxPooling2D((2, 2)))
         model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
         model.add(MaxPooling2D((2, 2)))
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+        model.add(Dense(10, activation='softmax'))
+        model.compile(optimizer=SGD(lr=0.001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
+        yield NeuralNetwork("Flat CNN", model)
+
+        model = Sequential()
+        model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)))
+        model.add(MaxPooling2D((2, 2)))
+        model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+        model.add(MaxPooling2D((2, 2)))
         model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
         model.add(MaxPooling2D((2, 2)))
         model.add(Flatten())
         model.add(Dense(128, activation='relu'))
         model.add(Dense(10, activation='softmax'))
-
-        opt = SGD(lr=0.001, momentum=0.9)
-        model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-
+        model.compile(optimizer=SGD(lr=0.001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
         yield NeuralNetwork("Simple CNN", model)
 
         model = Sequential()
@@ -35,8 +43,5 @@ class NeuralNetworkManager:
         model.add(Flatten())
         model.add(Dense(128, activation='relu'))
         model.add(Dense(10, activation='softmax'))
-
-        opt = SGD(lr=0.001, momentum=0.9)
-        model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-
+        model.compile(optimizer=SGD(lr=0.001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
         yield NeuralNetwork("Simple CNN with more filters", model)
