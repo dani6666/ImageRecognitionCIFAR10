112b57208fa5ba79d7c59a6cbed9dde3b90a286f
Daniel <danielwz99@gmail.com>
2021-02-14 20:07:14 +0100

    adding dropout to vgg

--- AllNeuralNetworks.py
+++ AllNeuralNetworks.py
@@ -1,11 +1,11 @@
 from keras import Sequential
-from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
+from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout
 from keras.optimizers import SGD
 
 
 class AllNeuralNetworks:
     networks_names = ["Simple CNN", "Flat CNN", "Simple CNN with more filters",
-                      "VGG 1 layer", "VGG 2 layer", "VGG 3 layer"]
+                      "VGG 1 layer", "VGG 2 layer", "VGG 3 layer", "VGG 3 layer with Dropout"]
 
     @staticmethod
     def get_network_model(name):
@@ -90,6 +90,28 @@ class AllNeuralNetworks:
             model.add(MaxPooling2D((2, 2)))
             model.add(Flatten())
             model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+            model.add(Dense(10, activation='softmax'))
+            model.compile(optimizer=SGD(lr=0.001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
+            return model
+
+        if name == AllNeuralNetworks.networks_names[6]:
+            model = Sequential()
+            model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same',
+                             input_shape=(32, 32, 3)))
+            model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+            model.add(MaxPooling2D((2, 2)))
+            model.add(Dropout(0.2))
+            model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+            model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+            model.add(MaxPooling2D((2, 2)))
+            model.add(Dropout(0.25))
+            model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+            model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+            model.add(MaxPooling2D((2, 2)))
+            model.add(Dropout(0.35))
+            model.add(Flatten())
+            model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+            model.add(Dropout(0.4))
             model.add(Dense(10, activation='softmax'))
             model.compile(optimizer=SGD(lr=0.001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
             return model
